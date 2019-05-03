bac42fc02a940c3a936e84170cda2d69bf209aa7
Unknown <nessijj@yahoo.se>
2019-05-03 15:30:24 +0200

    Dashboard/Navbar design.

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,43 +1,111 @@
-// This is our Navbar. It'll be fairly simple, but will handle all of our routing.
-
 // CSS and Material Design Imports
-import style from '../styles/Navbar.module.css';
-import Button from '@material-ui/core/Button';
+// import style from '../style/Navbar.module.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
 // Router and core functionality from react.
-import { BrowserRouter as Router, Route, NavLink, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import React, { Component } from 'react'
-
 
 // Existing component imports.
 import DashboardScreen from '../screens/DashboardScreen';
 import LoginScreen from '../screens/LoginScreen';
 
-// Handles all functionality for the Navbar and routing, including Navlinks,
-// Switch, Routes, and Redirect from /user to /login.
+// stuff for the new weather app
 
-export default class NavbarComponent extends Component {
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+
+const styles = {
+    menuButton: {
+        marginLeft: 650, //change this number to maximize the icon left
+
+    },
+};
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#212121',
+      }
+    }
+  })
+
+class NavbarComponent extends Component {
+
+    state = {
+        anchorEl: null,
+    };
+
+    handleChange = event => {
+        this.setState({ auth: event.target.checked });
+    };
+
+    handleMenu = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
     render() {
+        const { classes } = this.props;
+        const { anchorEl } = this.state;
+        const open = Boolean(anchorEl);
+
         return (
-            
-                <AppBar position="static" color="default">
-                    <Toolbar>
-                
-                <NavLink to="/login" activeClassName={style["active"]}>
-                            <div className={style["navbuttons"]}>
-                                <Button color="primary" size="large" >Login</Button>
+            <Router>
+                <MuiThemeProvider theme={theme}>
+                    <AppBar position="static" color="primary" style={{ background: "linear-gradient(to bottom, rgba(54, 56, 58, 0.062) 0%,rgba(55, 57, 58, 0.315) 100%)" }}>
+                        <Toolbar >
+                            <div>
+                                <IconButton
+                                    className={classes.menuButton}
+                                    aria-owns={open ? 'menu-appbar' : undefined}
+                                    aria-haspopup="true"
+                                    onClick={this.handleMenu}
+                                    color="inherit"
+                                >
+                                    <AccountCircle />
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={open}
+                                    onClose={this.handleClose}
+                                >
+                                    <MenuItem onClick={this.handleClose} ><Link to="/myaccount" style={{ textDecoration: 'none' }}>
+                                        My Account
+                                </Link></MenuItem>
+                                    <MenuItem onClick={this.handleClose} ><Link to="/dashboard" style={{ textDecoration: 'none' }}>
+                                        The Weather
+                                </Link></MenuItem>
+                                    <MenuItem onClick={this.handleClose} ><Link to="/login" style={{ textDecoration: 'none' }}>
+                                        Logout
+                                </Link></MenuItem>
+                                </Menu>
                             </div>
-                        </NavLink>
-                        <NavLink to="/dashboard" activeClassName={style["active"]}>
-                            <div className={style["navbuttons"]}>
-                                <Button color="primary" size="large" >Dashboard</Button>
-                            </div>
-                        </NavLink>
-                    </Toolbar>
-                </AppBar>
-            
+                        </Toolbar>
+                    </AppBar>
+                </MuiThemeProvider>
+            </Router>
+
         )
     }
 }
+
+export default withStyles(styles)(NavbarComponent);
