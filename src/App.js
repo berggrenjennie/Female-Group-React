05e683b280d4a463f3f05173517c9dcd1b9e5040
Unknown <nessijj@yahoo.se>
2019-05-05 16:42:01 +0200

    Final version with all major/minor bugs fixed. Added design/css to AccountComponent, UserComponent, and DashboardComponent. Refactored code. Added comments and reformatted. Deleted unnecessary screens.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 // Imports basic React funtionality. Imports Material UI's default typeface, which
 // we can change later to suit our app.
 // Router and core functionality from react.
-import { BrowserRouter, Route } from "react-router-dom";
-import React, { Component } from 'react'
-// Existing component imports.
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import NavbarComponent from './components/NavbarComponent';
-import DashboardScreen from './screens/DashboardScreen';
-import LoginScreen from './screens/LoginScreen';
-import './index.css';
-import 'typeface-roboto';
 // Renders Navbar Component, which is the main entry point to the app.
-class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <NavbarComponent/>
-          <Route path='/' exact component={LoginScreen} />
-          <Route path='/login' component={LoginScreen} /> 
-          <Route path='/dashboard' component={DashboardScreen} />  
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <NavbarComponent />
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -1,275 +1,306 @@
-// CSS and Material Design Imports
-
-// Router and core functionality from react.
-
-// Existing component imports.
-
-
-// The main functionality of our app. This is where the weather/date is displayed.
-// See "Screen 2 - The Weather" and "Screen 2 - The Weather (City)" in the
-// technical documentation. Routing will link to the Dashboard screen.
-// Grabs user info directly from local storage.
-
-import React, { Component } from 'react'
+// Core imports/Existing component imports.
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from "prop-types";
 
 // CSS and Material Design Imports
 import '../icons/weather.css';
 import style from '../styles/Dashboard.module.css';
 import LocationOn from '@material-ui/icons/LocationOn';
-
 import TextField from '@material-ui/core/TextField';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
-
-// Use HOC withHttp for weather fetch.
+// Use HOC imports: withHttp for weather fetch, withStorage for user information.
 import withHttp from './../services/withHttp';
-/* Interweave is a phenomenal library that is safe to toconvert a string text to HTML and
-insert it to the DOM. We use it when we chang the icon.*/
-import {Markup} from 'interweave';
+import withStorage from '../services/withStorage';
+
+/* Interweave is a phenomenal library that is safe to to convert a string text to HTML and
+insert it into the DOM. We use it when we change the icon.*/
+import { Markup } from 'interweave';
 
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#dfddda',
+      main: '#dfddda'
     }
   }
-})
+});
 
-let iconDiv="";
 class DashboardComponent extends Component {
-
-  // static propTypes = {
-  //   getWeather: PropTypes.func.isRequired// The function getWeather is required .
-  // }
-
+  static propTypes = {
+    history: PropTypes.object,
+    match: PropTypes.object,
+    location: PropTypes.object,
+    getUser: PropTypes.func,
+    getIcon: PropTypes.func,
+    getWeather: PropTypes.func,
+    getLocation: PropTypes.func
+  };
 
   constructor(props) {
     super(props);
-      this.state ={
-        searchLocaction: '',
-        locationData:[],
-        cityId:0,
-        iconData:[],
-        iconDiv:"",
-        weatherData: [],
-        isLoading: true,
-        errors: null,
-        tempUnit:"C",
-        showDiv: false
-        };
-      this.handlesearchLocaction = this.handlesearchLocaction.bind(this);
-    }
+    this.state = {
+      searchLocaction: '',
+      locationData: [],
+      cityId: 0,
+      iconData: [],
+      iconDiv: '',
+      weatherData: [],
+      cities: [],
+      isLoading: true,
+      errors: null,
+      showDiv: false,
+      user: {
+        name: '',
+        address: {
+          zipcode: 'C'
+        }
+      }
+    };
+  }
 
   /*A method that calls the function (getWeather()) and (getLocation()) which they are props received from withHttp component. */
+  componentDidMount() {
 
-    componentDidMount(){
-      this.props.getWeather('metric',this.difindCityId(this.state.CityId))
-        .then(response =>
-              // console.log(response.data)
-          this.setState({
-            weatherData:response.data,
-            isLoading: true
-          })
-        )
-       .catch(error => this.setState({ error, isLoading: false }));
-
-        this.props.getLocation()
-        .then(response =>
-                // console.log(response.data)
-          this.setState({
-            locationData:response.data
-          })
-        );
-
-        this.props.getIcon()
-        .then(response =>
-                 // console.log("icon:",response.data)
-          this.setState({
-            iconData:response.data
-          })
-         );
-       }
-
-
-    toggleDiv = () => {
-      this.setState(state => ({ showDiv: !state.showDiv }));
-      }
-
-/* A method that makes filter to locationData array (which have the data of all cities). In this filter we make compare
-between the searchCityInput(this consists of the name of the city and the country) and evry city's name in the arrary and
-return the cityId which will use sedan to get weatherData from API */
-  searchCity(searchCityInput){
-    const cityId = this.state.locationData.filter(function(item){
-      if(item.city.name===searchCityInput.slice(0,searchCityInput.indexOf(".")) &&
-        item.city.country===searchCityInput.slice(searchCityInput.indexOf(".")+1,searchCityInput.length).toUpperCase())
-          {
-            return item;
-          }
-      })
-
-      if(cityId.length > 0){
-
-        this.setState({cityId:Number(cityId[0].city.id.$numberLong)},
-        () => {
-          this.props.getWeather('metric',this.state.cityId)
-            .then(response => {
-              this.setState({
-                weatherData:response.data,
-                isLoading: true
-              })
-            })
-            .catch(error => this.setState({ error, isLoading: false }));
-         }
-      );
+    // If a user who is not logged in tries to access the dashboard, they are redirected
+    // to the dashboard.
+    const user = this.props.getUser();
+    if (user === null) {
+      this.props.history.push('/');
+      return;
     }
+
+    // This section sets the user, displays degrees depending on C/F and defaults city
+    // to Stockholm (city id from API). It then sets the weather to the response received from API.
+    // Note: Dashboard only cares about the user's name and the C/F (stored in zipcode).
+    this.setState({ user: user });
+    this.props
+      .getWeather(user.address.zipcode, 2673730)
+      .then(response =>
+        this.setState({
+          weatherData: response.data,
+          isLoading: true
+        })
+      )
+      .catch(error => this.setState({ error, isLoading: false }));
+
+    // Gets the location using function from withHttp and sets it as locationData state.
+    this.props.getLocation().then(response =>
+      this.setState({
+        locationData: response.data
+      })
+    );
+
+    // Gets the weather icon using function from withHttp and sets it as iconData state.
+    this.props.getIcon().then(response =>
+      this.setState({
+        iconData: response.data
+      })
+    );
   }
 
+  // This shows/hides the city search bar when a user clicks on the city.
+  toggleDiv = () => {
+    this.setState(state => ({ showDiv: !state.showDiv }));
+  };
 
-/* A method that controls and changes the value of the seach field in the form by using setState */
-  handlesearchLocaction(event) {
-    let searchCityInput= event.target.value;
-    this.setState({searchLocaction:searchCityInput});
+  // Filters locationData and compares searchCityInput (city, country), returns the city (or cities), and
+  // sets the city as a state.
+  searchCity(searchCityInput) {
+    if (searchCityInput.length === 0) {
+      return;
+    }
+
+    const cities = this.state.locationData.filter(function (item) {
+      if (item.city.name.toUpperCase() === searchCityInput.toUpperCase()) {
+        return true;
+      }
+      return false;
+    });
+
+    if (cities.length === 1) {
+      return this.setCity(cities[0]);
+    }
+    this.setState({ cities: [...cities] });
+  }
+
+  // Sets the cityId as a state, fetches the weather (with the help of withHttp) based on cityId and C/F (zipcode), and
+  // sets those states.
+  setCity = city => {
+    this.setState({ cityId: city.city.id.$numberLong }, () => {
+      this.props
+        .getWeather(this.state.user.address.zipcode, this.state.cityId)
+        .then(response => {
+          this.setState({
+            cities: [],
+            weatherData: response.data,
+            isLoading: true,
+            showDiv: !this.state.showDiv,
+            searchLocaction: ''
+          });
+        })
+        .catch(error => this.setState({ error, isLoading: false }));
+    });
+  };
+
+  // Controls and changes the value of the search field in the form by using setState.
+  handleSearchLocation = event => {
+    let searchCityInput = event.target.value;
+    this.setState({ searchLocaction: searchCityInput });
     this.searchCity(searchCityInput);
+  };
 
+  // Determines the icon which shows the weather condition.
+  getIconDiv(icon_Id) {
+    const icon = this.state.iconData.reduce((prev, icon) =>
+      icon.id === icon_Id ? icon.iconDiv : prev, undefined
+    );
 
-  }
-
-  difindCityId(id){
-    let city_Id=0;
-    if(id)
-      {
-        return city_Id=id;
-      }
-    return city_Id=2673730;
-  }
-
-  /* A method that uses to determine the icon which shows the weather condition .*/
-  getIconDiv(icon_Id){
-      const icon_ID = this.state.iconData.filter(function(icon){
-      if(icon.id===icon_Id)
-          {
-            return icon.iconDiv;
-          }
-      })
-
-      if(icon_ID.length>0)
-      {
-        iconDiv=icon_ID[0].iconDiv;
-      }
-          return iconDiv;
+    if (icon === undefined) {
+      return '';
     }
 
-/* A method that uses to determine the day. */
-  getDateName(date){
-      var options = { weekday: 'long', month: 'long', day: 'numeric' };
-      let day=new Date(date);
-      let dayName = day.toLocaleDateString("en-US", options)
-      return dayName;
+    return icon.id !== undefined ? icon.iconDiv : icon;
   }
 
-  render(){
+  // Formats the date according to the set options.
+  getDateName(date) {
+    var options = { weekday: 'long', month: 'long', day: 'numeric' };
+    let day = new Date(date);
+    let dayName = day.toLocaleDateString('en-US', options);
+    return dayName;
+  }
 
-    const {
-        list
-          } = this.state.weatherData;
+  // Multiple cities can come up depending on which city a user searches for (i.e. London has multiple intances).
+  // This sets the city the user clicks on.
+  onCityClick = city => e => {
+    e.preventDefault();
+    this.setCity(city);
+  };
 
-    const {
-        tempUnit,
-        weatherData,
-        isLoading
-          } = this.state;
+  render() {
+    const { list } = this.state.weatherData;
+
+    const { weatherData, user, cities } = this.state;
+    const tempUnit = user.address.zipcode.toUpperCase();
 
     return (
-
       <div className={style.card}>
-      
-        {/*Show username.*/}
         <div className={style.center}>
-          <p className={style.golduser}>Hello, Jennie!</p>
+          <p className={style.golduser}>Hello, {user.name}!</p>
         </div>
 
-        {list?
-        <div className={style.weather_display}>
-          {/*Show weather icon.*/}
-          <div className="icon" className={style.weather_icon}>
-          {console.log("divicon",list[0].weather[0].icon)}
-              < Markup content={this.getIconDiv(list[0].weather[0].icon)}/>
-          </div>
-          <div className={style.flex_container}>
-            {/*Show Current Day.*/}
-            <div className={style.silver}>{this.getDateName(list[0].dt_txt)}
-              {/* <div>{list[0].dt_txt}</div> */}
+        {list ? (
+          <div className={style.weather_display}>
+            <div className={`icon ${style.weather_icon}`}>
+              <Markup content={this.getIconDiv(list[0].weather[0].icon)} />
             </div>
-            <div>
-              {/*Show Current tempretur and it's unit.*/}
-              <p className={style.silver}>{list? list[0].main.temp+" "+tempUnit:null}</p>
+            <div className={style.flex_container}>
+              <div className={style.silver}>
+                {this.getDateName(list[0].dt_txt)}
+              </div>
+              <div>
+                <p className={style.silver}>
+                  {list ? Math.round(list[0].main.temp) + ' ' + tempUnit : null}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        :null}
+        ) : null}
 
-        {/*Show Location.*/}
         <div className={style.centerCity} onClick={this.toggleDiv}>
-          <p className={style.silvergold}><LocationOn />{weatherData.city? weatherData.city.name:null}</p>
+          <p className={style.silvergold}>
+            <LocationOn />
+            {weatherData.city ? weatherData.city.name : null}
+          </p>
         </div>
-        {/*Show seach fild.*/}
-        {this.state.showDiv &&
-        <div className={style.center}>
-          <MuiThemeProvider theme={theme}>
-            {/* <form >
-                <TextField label="Enter City" variant="outlined" type="text" placeholder="Search" value={this.state.searchLocaction} onChange={this.handlesearchLocaction}/>
-            </form> */}
-            <div className={style.center}>
-              <Grid container alignItems="flex-end">
-                <Grid item>
-                  <form >
-                    <TextField
-                    margin="normal"
-                    style={{ background: "linear-gradient(to bottom, rgba(233, 233, 233, 0.37) 0%,rgba(144, 144, 145, 0.089) 100%)" }}
-                    label="Enter City"
-                    variant="outlined"
-                    type="text"
-                    placeholder="Search"
-                    value={this.state.searchLocaction}
-                    onChange={this.handlesearchLocaction} />
-                  </form >
+        {this.state.showDiv && (
+          <div className={style.center}>
+            <MuiThemeProvider theme={theme}>
+              <div className={style.center}>
+                <Grid container alignItems='flex-end'>
+                  <Grid item>
+                    <form>
+                      <TextField
+                        margin='normal'
+                        style={{
+                          background:
+                            'linear-gradient(to bottom, rgba(233, 233, 233, 0.37) 0%,rgba(144, 144, 145, 0.089) 100%)'
+                        }}
+                        label='Enter City'
+                        variant='outlined'
+                        type='text'
+                        placeholder='Search'
+                        value={this.state.searchLocaction}
+                        onChange={this.handleSearchLocation}
+                      />
+                      {cities.map(city => {
+                        return (
+                          <li key={city.city.id.$numberLong}
+                            className={style.silvergoldlist}>
+                            <a
+                              href='/'
+                              key={city.city.id.$numberLong}
+                              onClick={this.onCityClick(city)}
+                            >
+                              {city.city.name}, {city.city.country}
+                            </a>
+                          </li>
+                        );
+                      })}
+                    </form>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </div>
+              </div>
             </MuiThemeProvider>
+            <div />
+          </div>
+        )}
+
+        {list ? (
+          <div className={style.flex_three}>
             <div>
-              {this.state.cityId?
-                  console.log("this.state.cityId",this.state.cityId)
-                :null}
+              <div className={`icon ${style.weather_icon}`}>
+                <Markup content={this.getIconDiv(list[5].weather[0].icon)} />
+              </div>
+              <div className={style.silver}>
+                {this.getDateName(list[5].dt_txt)}
+                <div className={style.silver}>
+                  {Math.round(list[5].main.temp) + ' ' + tempUnit}
+                </div>
+              </div>
             </div>
+
+            <div>
+              <div className={`icon ${style.weather_icon}`}>
+                <Markup content={this.getIconDiv(list[15].weather[0].icon)} />
+              </div>
+              <div className={style.silver}>
+                {this.getDateName(list[15].dt_txt)}
+                <div className={style.silver}>
+                  {Math.round(list[15].main.temp) + ' ' + tempUnit}
+                </div>
+              </div>
             </div>
-          }
 
-
-          {/*Show three days.*/}
-          {list?
-        <div className={style.flex_three}>
-          {/*Show First Day and it's tempretur.*/}
-
-          <div className={style.silver}>{this.getDateName(list[5].dt_txt)}
-            <div className={style.silver}>{list[5].main.temp+" "+tempUnit}</div>
+            <div>
+              <div className={`icon ${style.weather_icon}`}>
+                <Markup content={this.getIconDiv(list[25].weather[0].icon)} />
+              </div>
+              <div className={style.silver}>
+                {this.getDateName(list[25].dt_txt)}
+                <div className={style.silver}>
+                  {Math.round(list[25].main.temp) + ' ' + tempUnit}
+                </div>
+              </div>
+            </div>
           </div>
-          {/*Show Second Day and it's tempretur.*/}
-          <div className={style.silver}>{this.getDateName(list[15].dt_txt)}
-            <div className={style.silver}>{list[15].main.temp+" "+tempUnit}</div>
-          </div>
-          {/*Show Third Day and it's tempretur.*/}
-          <div className={style.silver}>{this.getDateName(list[25].dt_txt)}
-            <div className={style.silver}>{list[25].main.temp+" "+tempUnit}</div>
-          </div>
-        </div>
-        :null}
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
-
-export default withHttp(DashboardComponent);
+export default withRouter(withStorage(withHttp(DashboardComponent)));
